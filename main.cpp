472ecbca9c7a860673d0a1dd95321cec2316e6fb
Amogh Kalyan <80735346+amoghub@users.noreply.github.com>
2021-04-15 18:03:08 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,11 +4,19 @@ int main()
 {
     std::vector<std::vector<int>> vec
     {
-        // {1, 2, 5, 9, 3, 6},
-        // {6, 9, 4, 0, 1, 9},
-        // {4, 20, 1, 3, 7, 14},
-        // {3, 4, 8, 12, 5, 29},
-        // {9, 3, 1, 4, 6, 1}
+        /* 
+        Other vectors that can be used:
+        
+  1:    {1, 2, 5, 9, 3, 6},
+        {6, 9, 4, 0, 1, 9},
+        {4, 20, 1, 3, 7, 14},
+        {3, 4, 8, 12, 5, 29},
+        {9, 3, 1, 4, 6, 1}
+        
+  2:    {1, 2, 3}
+  
+        Try them out for yourself, or make some of your own! Make sure you use inputs that are within range of each vector!
+        */
 
         {1, 2, 3, 4, 5, 6},
         {1, 2, 3, 4, 5, 6},
@@ -16,16 +24,10 @@ int main()
         {1, 2, 3, 4, 5, 6},
         {1, 2, 3, 4, 5, 6}
 
-        //{1, 2, 3}
-
     };
-
-
-    std::cout << " bye" << std::endl;
+    
     Field hello(vec);
     std::cout << "Weight is: " << hello.Weight(1, 0, 4, 2) << std::endl;
     std::cout << "Pathcost is: " << hello.PathCost() << std::endl;
-    std::cout << " kai" << std::endl;
-
 
 }
